abac8e5cfa900b971e37c4cf7d6970d1325d5961
Alex Asriian <alexasr00798@gmail.com>
2026-02-24 19:26:53 +0200

    fix2

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,7 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.target.value || "Anonymous";
+  const trimValue = event.target.value.trim();
+  
+  nameOutput.textContent = trimValue || "Anonymous";
 });
