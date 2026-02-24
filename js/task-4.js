00f705140600a48be75ce9e8e73ee9d31e71a7fa
Alex Asriian <alexasr00798@gmail.com>
2026-02-24 19:31:15 +0200

    fix4

--- js/task-4.js
+++ js/task-4.js
@@ -3,8 +3,8 @@ const form = document.querySelector(".login-form")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const currentForm = event.target;
-    const email = form.elements.email.value.trim();
-    const password = form.elements.password.value.trim();
+    const email = currentForm.elements.email.value.trim();
+    const password = currentForm.elements.password.value.trim();
     
     if (email === "" || password === "") {
         return alert("Please fill in all the fields!");
